fe477252872b8b7adf7506bbd9ce408ac9f6c180
ClaireRoberts1403 <claireroberts1403@gmail.com>
2020-05-22 17:06:12 +0000

    JS var defined with html and css classes

---
+++ app.js
@@ -0,0 +1,23 @@
+let order = [];
+let playerOrder = [];
+let score;
+let level;
+let gameover;
+let compTurn;
+let start = false;
+let win;
+
+const runButton = document.querySelector('runButton');
+const scoreCount = document.querySelector('.scoreCount');
+const levelCount = document.querySelector('levelCount');
+const upOff = document.querySelector('upOff');
+const leftOff = document.querySelector('leftOff');
+const rightOff = document.querySelector('rightOff');
+// highscore const highScore = document.querySelector('highScore');
+
+//press run button
+
+runButton.addEventListener(onClick, (event) => {
+console.log("clicked")
+})
+
