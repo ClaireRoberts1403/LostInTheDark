c52fbe0f2ef7e63525eb9b9b39f967519041032e
ClaireRoberts1403 <claireroberts1403@gmail.com>
2020-05-22 17:43:06 +0000

    style class altered to match JS variable and first JS command complete and tested in console log

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let compTurn;
 let start = false;
 let win;
 
-const runButton = document.querySelector('runButton');
+const runButton = document.querySelector('.runButton');
 const scoreCount = document.querySelector('.scoreCount');
 const levelCount = document.querySelector('levelCount');
 const upOff = document.querySelector('upOff');
@@ -15,9 +15,18 @@ const leftOff = document.querySelector('leftOff');
 const rightOff = document.querySelector('rightOff');
 // highscore const highScore = document.querySelector('highScore');
 
-//press run button
+//press run button to start game
 
-runButton.addEventListener(onClick, (event) => {
-console.log("clicked")
-})
-
+runButton.addEventListener('click', play);
+    
+function play() {
+    win = false;
+    order = [];
+    playerOrder = [];
+    score = 0;
+    level = 0;
+    for (var i =0; i < 50; i++) {
+        order.push(Math.floor(Math.random() + 3) + 1);
+    }
+    console.log(order);
+}
