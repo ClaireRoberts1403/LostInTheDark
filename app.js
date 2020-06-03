d7a625b81c3c9301f42b06e1072d4e17482dff0c
ClaireRoberts1403 <claireroberts1403@gmail.com>
2020-06-03 17:23:12 +0000

    functions 123 defined and sounds added taken from the simon game credited in the readme as a stand in for now

--- app.js
+++ app.js
@@ -37,3 +37,52 @@ function play() {
   intervalId = setInterval(gameTurn, 800);
 }
 
+function gameTurn() {
+  on = false;
+
+  if (flash == level) {
+    clearInterval(intervalId);
+    compTurn = false;
+    clearColor();
+    on = true;
+  }
+
+  if (compTurn) {
+    clearColor();
+    setTimeout(() => {
+      if (order[flash] == 1) one();
+      if (order[flash] == 2) two();
+      if (order[flash] == 3) three();
+      flash++;
+    }, 200);
+  }
+}
+
+function one() {
+  if (noise) {
+    let audio = document.getElementById("clip1");
+    audio.play();
+  }
+  noise = true;
+  up.style.backgroundColor = "yellow";
+}
+
+function two() {
+  if (noise) {
+    let audio = document.getElementById("clip2");
+    audio.play();
+  }
+  noise = true;
+  left.style.backgroundColor = "yellow";
+}
+
+function three() {
+  if (noise) {
+    let audio = document.getElementById("clip3");
+    audio.play();
+  }
+  noise = true;
+  right.style.backgroundColor = "yellow";
+}
+
+
