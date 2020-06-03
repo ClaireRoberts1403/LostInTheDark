338ab5373497aacc3b722bd40820f061a0480025
ClaireRoberts1403 <claireroberts1403@gmail.com>
2020-06-03 18:16:55 +0000

    more css stylings complete game functions at a basic level

--- app.js
+++ app.js
@@ -146,7 +146,7 @@ function check() {
   if (playerOrder[playerOrder.length - 1] !== order[playerOrder.length - 1])
     good = false;
 
-  if (playerOrder.length == 5 && good) {
+  if (playerOrder.length == 50 && good) {
     winGame();
   }
 
